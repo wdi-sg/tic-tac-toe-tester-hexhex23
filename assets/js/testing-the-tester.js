4130f8b8d90fdc685fc97bb04bdfaa173d1d65ff
Maria <Maria>
2017-03-23 14:26:49 +0800

    tic tac toe js change

--- assets/js/testing-the-tester.js
+++ assets/js/testing-the-tester.js
@@ -1,60 +1,41 @@
-var grid =
-  [null, null, null,
-  null, null, null,
-  null, null, null]
-var currentPlayer = 1
-var sum = grid.reduce(function(acc,val) {
-  return acc + val
-})
-
-function restart() {
-  grid = [null, null, null, null, null, null, null, null, null]
-  currentPlayer = 1
-} restart ()
+var grid = [null, null, null, null, null, null, null, null, null]
+var player = 1
+function playTurn (index) {
+  if (grid[index] || isGameOver()) {
+    return false
+  } else {
+    grid[index] = player
+    if (player === 1) player = 2
+    else player = 1
+    return true
+  }
+}
 
 function isGameOver () {
-  switch (true) {
-    case (grid[0] === grid[1] && grid[1] === grid[2]): return true
-    break
-    case (grid[0] === grid[4] && grid[4] === grid[8]): return true
-    break
-    case (grid[2] === grid[4] && grid[4] === grid[6]): return true
-    break
-    case (grid[3] === grid[4] && grid[4] === grid[5]): return true
-    break
-    case (grid[6] === grid[7] && grid[7] === grid[8]): return true
-    break
-    case (grid[0] === grid[3] && grid[3] === grid[6]): return true
-    break
-    case (grid[1] === grid[4] && grid[4] === grid[7]): return true
-    break
-    case (grid[2] === grid[5] && grid[5] === grid[8]): return true
-    break
-    default:
-      return false
-  }
-} isGameOver ()
-
-function playTurn () {
-  if(isGameOver() !== true && sum === 3 || sum === 6 || sum === 9 || sum === 12) {
-    return currentPlayer = 1
-  } else if (isGameOver() !== true && sum === 1 || sum === 4 || sum === 7 || sum === 10) {
-    return currentPlayer = 2
-  } else if (isGameOver() !== true && grid[0] == null) {
-    return currentPlayer = 1
-  }
-
-} playTurn ()
+  if (whoWon()) return true
+  return false
+}
 
 function whoWon () {
-  if(isGameOver() === true) {
-    return currentPlayer
-  } else if(sum === 13) {
-    return 3
-  } else if (grid[index] === null){
-    return 0
-  }
-} whoWon ()
+  if (grid[0] && grid[0] === grid[1] && grid[0] === grid[2]) return grid[0]
+  if (grid[3] && grid[3] === grid[4] && grid[3] === grid[5]) return grid[3]
+  if (grid[6] && grid[6] === grid[7] && grid[6] === grid[8]) return grid[6]
+  if (grid[0] && grid[0] === grid[3] && grid[0] === grid[6]) return grid[0]
+  if (grid[1] && grid[1] === grid[4] && grid[1] === grid[7]) return grid[1]
+  if (grid[2] && grid[2] === grid[5] && grid[2] === grid[8]) return grid[2]
+  if (grid[0] && grid[0] === grid[4] && grid[0] === grid[8]) return grid[0]
+  if (grid[2] && grid[2] === grid[4] && grid[2] === grid[6]) return grid[2]
+  if (grid[0] && grid[1] && grid[2] && grid[3] && grid[4] &&
+    grid[5] && grid[6] && grid[7] && grid[8]) return 3
+  return 0
+}
+
+function restart () {
+  grid = [null, null, null, null, null, null, null, null, null]
+  player = 1
+}
+
+
 
 // $("document").ready(function() {
 //   $( ".grid" ).click(function() {
